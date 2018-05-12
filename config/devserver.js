2075aae6e43bc885cf71eda6225fd393d681b3f4
iceprosurface <nilibing@custouch.com>
2018-05-12 16:56:26 +0800

    [fix] fix code style for devserver
    修正 dev server 的 代码风格问题,添加 gitignore 相关信息

--- config/devserver.js
+++ config/devserver.js
@@ -1,7 +1,35 @@
 const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server')
-var config = require("./webpack.dev.js");
-config.entry.app.unshift("webpack-dev-server/client?http://localhost:8867/");
-var compiler = webpack(config);
-var server = new WebpackDevServer(compiler);
-server.listen(8867);
+const WebpackDevServer = require('webpack-dev-server');
+const http = require('http');
+let portStart = 8867;
+
+let config = require('./webpack.dev.js');
+config.entry.app.unshift('webpack-dev-server/client?http://localhost:8867/');
+let compiler = webpack(config);
+/**
+ * 查找可用端口
+ * @param {number} portStart
+ * @param {function} cb
+ */
+const getPort = function getPort(portStart, cb) {
+    let port = portStart;
+    let server = http.createServer();
+    server.listen(port, function() {
+        server.once('close', function() {
+            cb(port);
+        });
+        server.close();
+    });
+    server.on('error', function() {
+        getPort(portStart + 1, cb);
+    });
+};
+getPort(portStart, function(availablePort) {
+    const server = new WebpackDevServer(compiler, {
+        open: true,
+        hot: true,
+        host: 'localhost',
+    });
+    server.listen(availablePort);
+    console.log('server on localhost:' + availablePort);
+});
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,39 +1,39 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
+    mode: 'development',
     entry: {
-        'app': [path.resolve(__dirname, "../src/main.js")]
+        'app': [path.resolve(__dirname, '../src/main.js')],
     },
     output: {
         path: path.resolve(__dirname, '../build'),
-        publicPath: "./",
-        filename: 'js/[name][hash].js'
+        publicPath: './',
+        filename: 'js/[name][hash].js',
     },
     module: {
         rules: [{
             test: /\.scss$/,
-            loader: ['style-loader', 'css-loader', 'sass-loader']
+            loader: ['style-loader', 'css-loader', 'sass-loader'],
         }, {
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'
+            loader: 'babel-loader',
         }, {
             test: /\.html$/,
-            loader: "html-loader",
+            loader: 'html-loader',
         }, {
             test: /\.(png|jpg)$/,
-            loader: 'url-loader?limit=8192'
-        }]
+            loader: 'url-loader?limit=8192',
+        }],
     },
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: path.resolve(__dirname, './../index.html'),
             inject: true,
-            chunks: ['app']
+            chunks: ['app'],
         }),
-    ]
-}
+    ],
+};
